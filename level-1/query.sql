2dcb3e4a27e3319482c2a8de22fca35491d75d27
Your Name <you@example.com>
2022-01-26 19:52:14 -0800

    progress

---
+++ level-1/query.sql
@@ -0,0 +1,14 @@
+-- Count if the data is stored properly
+
+SELECT COUNT (*)
+FROM vine_table;
+
+SELECT COUNT (*)
+FROM customers;
+
+SELECT COUNT (*)
+FROM products;
+
+SELECT COUNT (*)
+FROM review_id_table;
+
